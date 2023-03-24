577bd2a0d0ea1defd4658ed18a3eb88100320331
Shuhrat Rahmonov <109937902+Shuhrat2022@users.noreply.github.com>
2023-03-24 11:02:12 +0500

    add new Item

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+System.Console.Write("Enter you 1st number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Enter you 2nd number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter you operation: \" + \" \" - \" \" * \" \" / \" \" % \" ");
+string operation = Console.ReadLine();
+
+//  ternary operator 
+string message = 
+    firstNumber >= 0 
+    ? "1st number is positive" 
+    : "1st number is negative"; 
+System.Console.WriteLine(message);
+if(firstNumber > secondNumber)
+{
+    System.Console.WriteLine("1st number is greater than 2nd number");
+}
+else if(firstNumber == secondNumber)
+{
+    System.Console.WriteLine("1st number is equal to 2nd number");
+}
+else
+{
+    System.Console.WriteLine("1st number is less than 2nd number");
+}
+switch(operation)
+{
+    case "+":System.Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");break;
+    case "-":System.Console.WriteLine($"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}");break;
+    case "*":System.Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}");break;
+    case "/":System.Console.WriteLine($"{firstNumber} / {secondNumber} = {firstNumber / secondNumber}");break;
+    case "%":System.Console.WriteLine($"{firstNumber} % {secondNumber} = {firstNumber % secondNumber}");break;
+    default: System.Console.WriteLine("Operation Not Found!");break;
+}
